8cc5214d4b98ffe1eb30e374f93dff6ed23233b8
Patrick Telnoni <ptelnoni89@gmail.com>
2025-11-03 10:43:46 +0700

    Catatan Update & Delete

---
+++ lib/sqlite/form_edit.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:ppbl/sqlite/connection.dart';
+import 'package:ppbl/sqlite/mahasiswa.dart';
+import 'package:ppbl/sqlite/read_data.dart';das
+
+class FormEdit extends StatefulWidget{
+  final Mahasiswa mahasiswa;
+
+  const FormEdit({super.key, required this.mahasiswa});
+
+  @override
+  State<FormEdit> createState() => _EditDataState(this.mahasiswa);
+}
+
+class _EditDataState extends State<FormEdit>{
+  final Mahasiswa mahasiswa;
+
+  _EditDataState(this.mahasiswa);
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
+
+  @override
+  void initState(){
+    super.initState();
+    nameController.text = mahasiswa.name;
+    ageController.text = mahasiswa.age.toString();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("SQLite Form Edit"),
+      ),
+      body:
+           Column(
+               children:[
+                 Padding(
+                   padding: const EdgeInsets.all(8.0),
+                   child: TextField(
+                     controller: nameController,
+                     decoration: InputDecoration(
+                       border: OutlineInputBorder(),
+                       labelText: "Nama",
+                     ),
+                   ),
+                 ),
+                 Padding(
+                   padding: const EdgeInsets.all(8.0),
+                   child: TextField(
+                     controller: ageController,
+                     decoration: InputDecoration(
+                       border: OutlineInputBorder(),
+                       labelText: "Umur",
+                     ),
+                   ),
+                 ),
+                 Padding(
+                     padding: const EdgeInsets.all(10.0),
+                     child: ElevatedButton(
+                       onPressed: () async {
+                         final db = await openMyDatabase();
+                         await db.update(
+                           'mahasiswa',
+                           {
+                             'name': nameController.text,
+                             'age': ageController.text,
+                           },
+                           where: '_id = ?',
+                           whereArgs: [mahasiswa.id],
+                         ).then((message){
+                           final snackBar = SnackBar(content: Text('Data berhasil diubah'));
+                           ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+                         }).catchError((error){
+                           final snackBar = SnackBar(content: Text('Terjadi kesalahan: $error'));
+                           ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                         });
+                         },
+                       child: const Text("Update"),
+                     )
+                 ),
+               ]
+           ),
+    );
+  }
+}
--- lib/sqlite/read_data.dart
+++ lib/sqlite/read_data.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ppbl/sqlite/mahasiswa.dart';
+import 'package:ppbl/sqlite/form_edit.dart';
 import 'package:ppbl/sqlite/connection.dart';
 
 Future<List<Mahasiswa>> fetchMahasiswa() async{
@@ -55,10 +56,36 @@ class _ReadDataState extends State<ReadData>{
               return ListView.builder(
                 itemCount: futureMahasiswa.length,
                 itemBuilder: (context, index){
-                  return ListTile(
-                    leading: Text(futureMahasiswa[index].id.toString()),
-                    title: Text(futureMahasiswa[index].name),
-                    subtitle: Text(futureMahasiswa[index].age.toString()),
+                  return GestureDetector(
+                    onTap: (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => FormEdit(mahasiswa:futureMahasiswa[index])
+                        ),
+                      );
+                    },
+                      onLongPress:  () async{
+                        final db = await openMyDatabase();
+                        db.delete('mahasiswa', where: '_id = ?', whereArgs: [futureMahasiswa[index].id])
+                            .then((message){
+                          final snackBar = SnackBar(content: Text('Data berhasil dihapus'));
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                          setState(() {
+                            futureMahasiswa.removeAt(index);
+                          });
+                        })
+                            .catchError((err){
+                          final snackBar = SnackBar(content: Text('Terjadi kesalahan: $err'));
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        });
+                      },
+                    child:ListTile(
+                      leading: Text(futureMahasiswa[index].id.toString()),
+                      title: Text(futureMahasiswa[index].name),
+                      subtitle: Text(futureMahasiswa[index].age.toString()),
+
+                    )
                   );
                 },
               );
